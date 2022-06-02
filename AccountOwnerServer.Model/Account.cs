4f2fadc4db170a669dc569e704510f9a99ad22d3
Hugo Rodríguez <hugsrodriguez8@gmail.com>
2022-06-01 23:58:42 -0600

    Agregando Models

---
+++ AccountOwnerServer.Model/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer.Model
+{
+    public class Account
+    {
+        private string accountId;
+        private DateTime date;
+        private string accountType;
+        private string ownerId;
+
+        public string AccountId { get => accountId; set => accountId = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string AccountType { get => accountType; set => accountType = value; }
+        public string OwnerId { get => ownerId; set => ownerId = value; }
+    }
+}
---
+++ AccountOwnerServer.Model/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer.Model
+{
+    public class Owner
+    {
+        private string ownerId;
+        private string name;
+        private DateTime date;
+        private string address;
+
+        public string OwnerId { get => ownerId; set => ownerId = value; }
+        public string Name { get => name; set => name = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Addres { get => address; set => address = value; }
+    }
+}
